6f045c0ffd7ac02aae1b0b421e1c3496e5039455
Carlos García Trujillo <cgarciagl@gmail.com>
2025-06-09 14:45:11 -0600

    Modificar clases de margen en dropdownfield y multiselectfield para mantener la consistencia en el diseño

--- app/ThirdParty/Ragnos/Views/rfield/multiselectfield.php
+++ app/ThirdParty/Ragnos/Views/rfield/multiselectfield.php
@@ -1,4 +1,4 @@
-<div class="divfield col-sm-4 mt-1">
+<div class="divfield col-sm-4 mb-1">
     <div class='form-control shadow-sm' id='group_<?= $name ?>'>
         <label class="control-label">
             <?= $label; ?> :
